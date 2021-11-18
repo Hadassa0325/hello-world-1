53545c46af90efdaf123a1e5991bbdfb2db3e51f
unknown <claudeesther.g@gmail.com>
2021-11-18 16:58:29 +0100

    cours js hello world

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+
+<!--template DOM Htlm -->
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> <!-- quand ça commence par une lettre en maj c'est que c'est un composant -->
+</template> 
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1> Tab name  </h1>
+    
+    <ul id="example-1">
+      <li v-for="item in items" :key="item.message"> 
+           {{ item.message }}</li>
+           </ul>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+data () {
+  return{ 
+  items: [   
+       { message: 'GOMA Claude Esther' }, 
+       { message: 'GOMA Claude Esther'}
+  ]
+}
+},
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
